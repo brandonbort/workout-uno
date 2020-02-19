304d263d49d43d39098f5134afed62e4ecd8e306
Brandon Bort <60452233+brandonbort@users.noreply.github.com>
2020-02-18 18:49:16 -0600

    Add files via upload

---
+++ src/workoutuno/WorkoutUno.java
@@ -0,0 +1,21 @@
+/*
+ * AUTHOR: Brandon Bort
+ * FOR: CS 2365
+ * To change this template file, choose Tools | Templates
+ */
+package workoutuno;
+
+/**
+ *
+ * @author brand
+ */
+public class WorkoutUno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
