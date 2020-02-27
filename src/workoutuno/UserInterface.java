031a46b807bbd166e858cd82b615a9a0a2de3873
brand <brand@10.161.155.103>
2020-02-27 16:49:52 -0600

    Created basic user interface

--- src/workoutuno/UserInterface.java
+++ src/workoutuno/UserInterface.java
@@ -6,6 +6,8 @@
 
 package workoutuno;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import javax.swing.*;
 /**
  *
@@ -16,31 +18,47 @@ public class UserInterface {
     public static int[] getOptions(){
         int options[] = new int[3];
         JFrame parFrame = new JFrame();
+//        FlowLayout layout = new FlowLayout();
+        
         //creating a user interface 
-//        ButtonGroup numOfDecks = new ButtonGroup();
-        JFrame numOfDecks = new JFrame();
+        JPanel child = new JPanel();
+        JPanel radios = new JPanel();
         JRadioButton one = new JRadioButton("1");
         JRadioButton two = new JRadioButton("2");
         JRadioButton three = new JRadioButton("3");
+        radios.setBorder(BorderFactory.createTitledBorder("Number of Decks"));
+        
         //adding creating radio buttons to the button groups
-        numOfDecks.add(one);
-        numOfDecks.add(two);
-        numOfDecks.add(three);
-        parFrame.add(numOfDecks);
+        radios.add(one);
+        radios.add(two);
+        radios.add(three);
         
         //button group for options for shuffling
-        JFrame shuffleOpts = new JFrame();
+        JPanel shuffleOpts = new JPanel(new GridLayout(2, 2));
         JRadioButton alone = new JRadioButton("Shuffle Decks individually");
         JRadioButton together = new JRadioButton("Shuffle decks together");
         shuffleOpts.add(alone);
         shuffleOpts.add(together);
-        parFrame.add(shuffleOpts);
-       
+//        shuffleOpts.setLayout(layout);
+        
+        JPanel finalOpts = new JPanel(new GridLayout(2,2));
         JCheckBox inclAct = new JCheckBox("Include action cards", true);
-        parFrame.add(inclAct);
+        JButton okayBtn = new JButton("Okay");
+        finalOpts.add(inclAct);
+        finalOpts.add(okayBtn);
         
+        child.add(radios, BorderLayout.NORTH);
+        child.add(shuffleOpts);
+        child.add(finalOpts);
+            
+        parFrame.add(child);
         
-        
+        //making the UI visible
+        ImageIcon img = new ImageIcon(System.getProperty("user.dir") + "\\utility\\uno-logo.png");
+        parFrame.setIconImage(img.getImage());
+        parFrame.setVisible(true);
+        parFrame.setSize(200,225);
+
         
         return options;
     }
