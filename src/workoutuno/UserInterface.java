51418d2eea7f41324608f1bb60be94abe09d3938
brand <brand@10.161.155.103>
2020-03-02 15:14:13 -0600

    Added multi-threading for UI, pausing execution of main thread until the "Okay" button is clicked

--- src/workoutuno/UserInterface.java
+++ src/workoutuno/UserInterface.java
@@ -8,42 +8,76 @@ package workoutuno;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.*;
 import javax.swing.*;
 /**
  *
  * Author: BRANDON BORT
  */
-public class UserInterface {
+class UserInterface extends JFrame{
+    
+    public void getOptions(int[] options, Object lock){
 
-    public static int[] getOptions(){
-        int options[] = new int[3];
-        JFrame parFrame = new JFrame();
-//        FlowLayout layout = new FlowLayout();
+        UserInterface parFrame = new UserInterface();
         
         //creating a user interface 
         JPanel child = new JPanel();
         JPanel radios = new JPanel();
+        ButtonGroup radioGroup = new ButtonGroup();
         JRadioButton one = new JRadioButton("1");
         JRadioButton two = new JRadioButton("2");
         JRadioButton three = new JRadioButton("3");
         radios.setBorder(BorderFactory.createTitledBorder("Number of Decks"));
         
-        //adding creating radio buttons to the button groups
+        //adding the buttons to the radio group insures only one can be selected at a time
+        radioGroup.add(one);
+        radioGroup.add(two);
+        radioGroup.add(three);
+        radioGroup.setSelected(one.getModel(), true); //sets button one as default selected to prevent errors
         radios.add(one);
         radios.add(two);
         radios.add(three);
         
         //button group for options for shuffling
         JPanel shuffleOpts = new JPanel(new GridLayout(2, 2));
+        ButtonGroup shuffleGroup = new ButtonGroup();
         JRadioButton alone = new JRadioButton("Shuffle Decks individually");
         JRadioButton together = new JRadioButton("Shuffle decks together");
+        
+        shuffleGroup.add(alone);
+        shuffleGroup.add(together);
+        //sets shuffling alone as the default option
+        shuffleGroup.setSelected(alone.getModel(), true);
         shuffleOpts.add(alone);
         shuffleOpts.add(together);
-//        shuffleOpts.setLayout(layout);
-        
+                
         JPanel finalOpts = new JPanel(new GridLayout(2,2));
         JCheckBox inclAct = new JCheckBox("Include action cards", true);
         JButton okayBtn = new JButton("Okay");
+        //sets the Okay button to get the results from the UI
+
+        okayBtn.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e)
+            {
+              if(e.getSource() == okayBtn)  {
+
+                if(radioGroup.getSelection() == one.getModel()) options[0] = 1;
+                else if(radioGroup.getSelection() == two.getModel()) options[0] = 2;
+                else if(radioGroup.getSelection() == three.getModel()) options[0] = 3;
+                //sets to 0 if the user selected to shuffle each deck alone, or 1 if together
+                if(shuffleGroup.getSelection() == alone.getModel()) options[1] = 0;
+                else if(shuffleGroup.getSelection() == together.getModel()) options[1] = 1;
+                //set to 0 if the user wants action cards included, or 1 if they don't
+                if(inclAct.isSelected()) options[2] = 0;
+                else options[2] = 1;
+                synchronized(lock){
+                    lock.notify();
+                }//the main thread will resume upon the options array being populated
+                
+                parFrame.dispatchEvent(new WindowEvent(parFrame, WindowEvent.WINDOW_CLOSING));
+              }
+            }
+        });
         finalOpts.add(inclAct);
         finalOpts.add(okayBtn);
         
@@ -58,9 +92,8 @@ public class UserInterface {
         parFrame.setIconImage(img.getImage());
         parFrame.setVisible(true);
         parFrame.setSize(200,225);
-
         
-        return options;
+        parFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
-    
 }
