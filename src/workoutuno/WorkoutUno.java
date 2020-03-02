aa83a521b6815955ea9d3a2560f85d57448a66a0
brand <brand@10.161.155.103>
2020-03-02 15:21:11 -0600

    Added multi-threading for UI, pausing execution of main thread until the "Okay" button is clicked

--- src/workoutuno/WorkoutUno.java
+++ src/workoutuno/WorkoutUno.java
@@ -5,6 +5,8 @@
  */
 package workoutuno;
 
+import static java.lang.System.exit;
+
 /**
  *
  * @author brand
@@ -14,8 +16,32 @@ public class WorkoutUno {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
         // TODO code application logic here
+       UserInterface ui = new UserInterface();
+       int[] options = new int[3];
+       Object lock = new Object(); //lock object will pause thread until user enters input in GUI
+       ui.getOptions(options, lock);//passes the array to populate with choices, and lock that pauses the thread
+       synchronized(lock) {
+            try {
+                // Calling wait() will block this thread until user presses the
+                // okayBtn in the UI
+                lock.wait();
+            } catch (InterruptedException e) {
+            // Happens if someone interrupts the lock thread
+            }
+       }
+       //options[0] will return the number of decks the user selected
+       //options[1] will return a 0 if the user selcted to shuffle the decks individually
+       //options[1] will return a 1 if the user selected to shuffle the decks together
+       //options[2] will return a 0 if the user selected to include action cards,
+       //or a 1 if they chose not to
+       exit(0);
     }
+    
+    public static void shuffle(){
+
+    }
+
     
 }
