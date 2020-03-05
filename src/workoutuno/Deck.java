d73c1d6187fb8312cdfd50a0a4afc60f38db7dee
brand <brand@10.161.155.103>
2020-03-05 16:49:50 -0600

    Added in unfinished sorting methods

--- src/workoutuno/Deck.java
+++ src/workoutuno/Deck.java
@@ -1,7 +1,9 @@
 /*
  * AUTHOR: Aaron Sloan
  * FOR: CS 2365
- *
+ * COLLABORATOR: Brandon Bort
+ * CHANGES MADE: added a cardCount int, with associated getters and setters. Also
+ * changed push and pop functions to change the variable
  */
 package workoutuno;
 
@@ -11,6 +13,7 @@ package workoutuno;
 public class Deck { 
 
     Node head; // head of list
+    private int cardCount;
     
     static class Node {
         Card card;
@@ -35,7 +38,8 @@ public class Deck {
   
         // If head needs to be removed 
         head = temp.next;   // Change head 
-        
+        //removes one from the cardCount
+        setCardCount(-1);
         
         return temp.card;
     } 
@@ -61,7 +65,33 @@ public class Deck {
   
             // Insert the new_node at last node 
             last.next = new_node; 
-        } 
+        }
+        setCardCount(1);
     } 
+    //BRBORT- added function specifically for combining multiple decks together
+    public void pushDeck(Deck new_deck){
+            Node last = head; 
+            while (last.next != null) { 
+                last = last.next; 
+            } 
+            
+            // Insert the new_node at last node 
+            last.next = new_deck.head;
+            last = last.next;
+            setCardCount(new_deck.getCardCount());
+    }
+    
+    public void setCardCount(int card){
+        if(card < 0 && getCardCount() < 0){
+            System.out.println("Error: Cannot have negative cards in deck.");
+        }
+        else{
+            this.cardCount += card;
+        }
+    }
+    
+    public int getCardCount(){
+        return this.cardCount;
+    }
    
 } 
